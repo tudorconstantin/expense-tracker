9bd530b96844a33b9881e47906e8de3269c88071
Tudor Constantin <tudorconstantin@gmail.com>
2012-07-08 13:16:27 +0300

    namespaced controllers under ExpenseTracker::Controllers

---
+++ lib/ExpenseTracker/Controllers/Site.pm
@@ -0,0 +1,10 @@
+package ExpenseTracker::Controllers::Site;
+use Mojo::Base 'Mojolicious::Controller';
+
+sub welcome{
+  my $self = shift;
+  
+  return $self->render( message => 'Hello World' );
+}
+
+1;
