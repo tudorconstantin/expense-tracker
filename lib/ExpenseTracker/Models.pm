cf16c3726bb34be0b9beb375f3a069b02feaec46
Tudor Constantin <tudorconstantin@gmail.com>
2012-07-08 11:28:27 +0300

    generated dbic "models" based on the db structure

---
+++ lib/ExpenseTracker/Models.pm
@@ -0,0 +1,20 @@
+use utf8;
+package ExpenseTracker::Models;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07017 @ 2012-07-08 11:25:25
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ubhSbC7TLkFYO9posDuqWw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/ExpenseTracker/Models/Result/User.pm
@@ -0,0 +1,132 @@
+use utf8;
+package ExpenseTracker::Models::Result::User;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+ExpenseTracker::Models::Result::User
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<users>
+
+=cut
+
+__PACKAGE__->table("users");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 username
+
+  data_type: 'varchar'
+  is_nullable: 0
+
+=head2 password
+
+  data_type: 'char'
+  is_nullable: 0
+
+=head2 email
+
+  data_type: 'varchar'
+  is_nullable: 0
+
+=head2 first_name
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 last_name
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 birth_date
+
+  data_type: 'datetime'
+  is_nullable: 1
+
+=head2 created_at
+
+  data_type: 'datetime'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "username",
+  { data_type => "varchar", is_nullable => 0 },
+  "password",
+  { data_type => "char", is_nullable => 0 },
+  "email",
+  { data_type => "varchar", is_nullable => 0 },
+  "first_name",
+  { data_type => "varchar", is_nullable => 1 },
+  "last_name",
+  { data_type => "varchar", is_nullable => 1 },
+  "birth_date",
+  { data_type => "datetime", is_nullable => 1 },
+  "created_at",
+  { data_type => "datetime", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<email_unique>
+
+=over 4
+
+=item * L</email>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("email_unique", ["email"]);
+
+=head2 C<username_unique>
+
+=over 4
+
+=item * L</username>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("username_unique", ["username"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07017 @ 2012-07-08 11:25:25
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:JewiMTxAfKfxnGlQAXq/AA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
