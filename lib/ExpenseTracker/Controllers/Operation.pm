88ae652e57af82daa7738643992584ef72d6de45
Tudor Constantin <tudorconstantin@gmail.com>
2012-11-09 18:16:01 +0200

    listing expenses by categories

--- lib/ExpenseTracker/Controllers/Operation.pm
+++ lib/ExpenseTracker/Controllers/Operation.pm
@@ -1,29 +1,34 @@
 package ExpenseTracker::Controllers::Operation;
 use Mojo::Base 'ExpenseTracker::Controllers::Base';
 
-sub new{
+sub new {
   my $self = shift;
-  
+
   my $obj = $self->SUPER::new(@_);
-  
+
   $obj->{resource} = 'ExpenseTracker::Models::Result::Operation';
 
   return $obj;
-  
+
 }
 
-sub list_by_category{
-  my ( $self ) = @_;
+sub list_by_category {
+  my ($self) = @_;
 
-  my $result_rs = $self->app->user->categories( { 'me.id' => $self->param('category_id') } )->search_related_rs( 'operations_category' )->search_related_rs( 'operation' );
-  
-  $result_rs->result_class('DBIx::Class::ResultClass::HashRefInflator');     
+  my $result_rs =
+    $self->app->user->categories(
+        {
+          'me.id' => $self->param('category_id'),
+        } )
+    ->search_related_rs('operations_category')
+    ->search_related_rs('operation');
 
-  my @expenses = $result_rs->all ;
-  return $self->render_not_found() if scalar @expenses == 0 ;
-  return $self->render_json( [ @expenses ] );  
-  
+  $result_rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+
+  my @expenses = $result_rs->all;
+  return $self->render_not_found() if scalar @expenses == 0;
+  return $self->render_json( [@expenses] );
+
 }
-
 
 1;
